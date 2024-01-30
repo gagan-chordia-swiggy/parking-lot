bc4c693e071d7fc13eba56481261519ea6fee385
gagan-chordia-swiggy <gagan.chordia_ftc@external.swiggy.in>
2024-01-30 09:48:35 +0530

    31. Added test to check if the negative level ticket is not created and its implementation

--- src/main/java/org/example/Ticket.java
+++ src/main/java/org/example/Ticket.java
@@ -1,4 +1,9 @@
 package org.example;
 
 public record Ticket(int level, int slot, String registrationNumber) {
+    public Ticket {
+        if (level < 0) {
+            throw new InvalidTicketException();
+        }
+    }
 }
--- src/test/java/TicketTest.java
+++ src/test/java/TicketTest.java
@@ -1,7 +1,9 @@
+import org.example.InvalidTicketException;
 import org.example.Ticket;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TicketTest {
     @Test
@@ -11,5 +13,11 @@ public class TicketTest {
 
         // Assert
         assertNotNull(ticket);
+    }
+
+    @Test
+    void testNegativeLevelTicketThrowsException() {
+        // Assert
+        assertThrows(InvalidTicketException.class, () -> new Ticket(-1, 3, "AB12BC1234"));
     }
 }
